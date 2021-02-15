84a164585a3edc0b307c3e763ced270797437e7c
Jack Latimer <jacklatimer03@gmail.com>
2021-02-15 02:23:46 +0000

    nine4

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,14 @@ export default function Container() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
 
   return (
-    <div class="fixed z-50 w-full fh52u">
-      <div class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-        <div class="flex flex-row items-center justify-between p-4">
+    <div className="fixed z-50 w-full fh52u">
+      <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+        <div className="flex flex-row items-center justify-between p-4">
           <a
             href="#"
-            class="text-lg font-semibold rounded-lg tracking-widestuppercase focus:outline-none focus:shadow-outline"
+            className="text-lg font-semibold rounded-lg tracking-widestuppercase focus:outline-none focus:shadow-outline"
           >
-            <h1 class="text-2xl font-bold tracking-tighter text-white uppercase md:text-sm lg:text-3xl">
+            <h1 className="text-2xl font-bold tracking-tighter text-white uppercase md:text-sm lg:text-3xl">
               NINE4
             </h1>
           </a>
@@ -28,10 +28,10 @@ export default function Container() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="white"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-menu"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-menu"
             >
               <line x1="3" y1="12" x2="21" y2="12"></line>
               <line x1="3" y1="6" x2="21" y2="6"></line>
@@ -46,20 +46,20 @@ export default function Container() {
           }
           id="example-navbar-danger"
         >
-          <nav class="flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+          <nav className="flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
             <a
-              class="px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-lg md:mt-0 md:ml-4 focus:text-white hover:bg-white hover:text-black focus:outline-none focus:shadow-outline"
+              className="px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent rounded-lg text-lg md:mt-0 md:ml-4 focus:text-white hover:bg-white hover:text-black focus:outline-none focus:shadow-outline"
               href="/"
             >
               Docs
             </a>
 
             <a
-              class="inline-flex items-center px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent gwe62 border-gray-800 border rounded-lg text-md md:mt-0 md:ml-4 hover:text-black hover:bg-white focus:shadow-outline"
+              className="inline-flex items-center px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent gwe62 border-gray-800 border rounded-lg text-md md:mt-0 md:ml-4 hover:text-black hover:bg-white focus:shadow-outline"
               href="/"
             >
-              <div class="flex text-lg tracking-tighter">
-                <span class="justify-center">View Templates</span>
+              <div className="flex text-lg tracking-tighter">
+                <span className="justify-center">View Templates</span>
               </div>
             </a>
           </nav>
