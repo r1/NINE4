7a82c27bb7ace4663dfdf92fe1dcdf3b8601daa2
Jack Latimer <jacklatimer03@gmail.com>
2021-02-15 02:23:46 +0000

    nine4

--- components/Main.js
+++ components/Main.js
@@ -2,10 +2,10 @@ import TextLoop from "react-text-loop";
 
 export default function Main() {
   return (
-    <section class="text-black body-font lg:pt-20">
-      <div class="container px-5 mx-auto lg:px-4 lg:py-4">
-        <div class="flex flex-col w-full mb-2 text-left md:text-center mt-16">
-          <h1 class="mb-1 text-6xl font-bold tracking-tighter text-center text-white lg:text-8xl md:text-7xl">
+    <section className="text-black body-font lg:pt-20">
+      <div className="container px-5 mx-auto lg:px-4 lg:py-4">
+        <div className="flex flex-col w-full mb-2 text-left md:text-center mt-16">
+          <h1 className="mb-1 text-6xl font-bold tracking-tighter text-center text-white lg:text-8xl md:text-7xl">
             A free template site for{" "}
             <TextLoop className="tr62">
               <span className="text-purple-600">Developers</span>
@@ -15,15 +15,15 @@ export default function Main() {
             
           </h1>
           <br></br>
-          <p class="mx-auto text-xl font-normal leading-relaxed fs521 dark:text-gray-300 lg:w-2/3">
+          <p className="mx-auto text-xl font-normal leading-relaxed fs521 dark:text-gray-300 lg:w-2/3">
             nine4 is a free to use template website for websites made with
             Next.js and styled with Tailwind CSS
           </p>
         </div>
       </div>
-      <div class="container flex flex-col items-center justify-center mx-auto rounded-lg ">
+      <div className="container flex flex-col items-center justify-center mx-auto rounded-lg ">
         <img
-          class="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
+          className="object-cover object-center w-3/4 mb-10 g327 border rounded-lg shadow-md"
           alt="hero"
           src="./images/nine4-1.png"
         ></img>
