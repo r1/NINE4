2b07f0f971afcfbde7b75844a34afeac21f1f17c
Jack Latimer <jacklatimer03@gmail.com>
2021-02-15 01:09:30 +0000

    nine4

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ export default function Container() {
             </h1>
           </a>
           <button
-            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none"
             type="button"
             onClick={() => setNavbarOpen(!navbarOpen)}
           >
@@ -41,7 +41,7 @@ export default function Container() {
         </div>
         <div
           className={
-            "lg:flex flex-grow items-center" +
+            "md:flex flex-grow items-center" +
             (navbarOpen ? " flex" : " hidden")
           }
           id="example-navbar-danger"
