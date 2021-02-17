03391dbddbeaf5e6bac773cebedc4f52395fdf6a
Jack Latimer <jacklatimer03@gmail.com>
2021-02-17 05:28:51 +0000

    Analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,11 +1,9 @@
 import ReactGA from 'react-ga'
 
 export const initGA = () => {
-  console.log('GA init')
   ReactGA.initialize('UA-152031643-1')
 }
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`)
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
