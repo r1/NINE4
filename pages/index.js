995a11bd1e0707f05d2165a2ce11d1866adf9995
Jack Latimer <jacklatimer03@gmail.com>
2021-02-17 05:36:51 +0000

    Analytics

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,23 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import { NextSeo } from "next-seo";
-import Layout from "@/components/Layout"
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
     <Layout>
-    <body className="text-white">
-      <Head>
-        <title>nine4</title>
-        <meta name="keywords" content="nine4, Jack Latimer, r1"></meta>
-      </Head>
-      <NextSeo
-        title="nine4"
-        description="Free Template Sites Built With Next.js & Styled With TailwindCSS"
-      />
-      <Header />
-      <Main />
-    </body>
+      <body className="text-white">
+        <Head>
+          <title>nine4</title>
+          <meta name="keywords" content="nine4, Jack Latimer, r1"></meta>
+        </Head>
+        <NextSeo
+          title="nine4"
+          description="Free Template Sites Built With Next.js & Styled With TailwindCSS"
+        />
+        <Header />
+        <Main />
+      </body>
     </Layout>
   );
 }
