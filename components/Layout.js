fc1914f81a19dd87ed73d39eacc277804a80f293
Jack Latimer <jacklatimer03@gmail.com>
2021-02-18 06:50:42 +0000

    Remove Text Scroll

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,11 @@
-import TextLoop from "react-text-loop";
-
 export default function Main() {
   return (
     <section className="text-black body-font lg:pt-14">
       <div className="container px-5 mx-auto lg:px-4 lg:py-4">
         <div className="flex flex-col w-full mb-2 text-left md:text-center ">
           <h1 className="mb-1 text-6xl font-bold tracking-tighter text-center text-white lg:text-8xl md:text-7xl">
-            A free template site for{" "}
-            <TextLoop>
-              <span className="text-purple-600">Developers‏‏‎</span>
-              <span className="text-blue-600">Businesses</span>
-              <span className="text-red-600">Hobbyists‏‏‎‏‏</span>
-            </TextLoop>
+            Free template sites for{" "}
+              <span className="text-blue-600">Everyone</span>
           </h1>
           <br></br>
           <p className="mx-auto text-xl font-normal leading-relaxed fs521 lg:w-2/3">
