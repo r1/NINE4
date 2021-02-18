0413d9f18198a1851c4519422c0a1f7c33375ad3
Jack Latimer <jacklatimer03@gmail.com>
2021-02-18 05:47:53 +0000

    Site Colours

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Container() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
 
   return (
-    <div className="z-50 w-full fh52u">
+    <div className="z-50 w-full">
       <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="flex flex-row items-center justify-between p-4">
           <a
@@ -54,7 +54,7 @@ export default function Container() {
             </a>
 
             <a
-              className="inline-flex items-center px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent gwe62 border-gray-800 border rounded-lg text-md md:mt-0 md:ml-4 hover:text-black hover:bg-white focus:shadow-outline"
+              className="inline-flex items-center px-4 py-2 mt-2 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-transparent gwe62 g327 border rounded-lg text-md md:mt-0 md:ml-4 hover:text-black hover:bg-white focus:shadow-outline"
               href="/"
             >
               <div className="flex text-lg tracking-tighter">
