87b9b9fb1d7a6d688f4e447cb6893c15af8077f9
Jack Latimer <jacklatimer03@gmail.com>
2021-02-23 13:49:12 +0000

    Delete analytics key

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,7 +2,7 @@ import ReactGA from 'react-ga'
 
 export const initGA = () => {
 // https://gyazo.com/3101aeec0a80c2e637e8856449ddf071 Select these settings when you are setting up your analytics to get a tracking ID
-  ReactGA.initialize('UA-152031643-1')
+  ReactGA.initialize('UA-XXXXXXXXX-1')
 }
 
 export const logPageView = () => {
