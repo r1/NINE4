b28d89e84e09e6cfdb502962da453f8bbba0d4dc
Jack Latimer <jacklatimer03@gmail.com>
2021-02-24 00:50:02 +0000

    done(analytics key): Vercel enviroment variables

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+      GA_ENV: process.env.GA_ENV,
+    },
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: "empty",
+        };
+      }
+  
+      return config;
+    },
+  };
