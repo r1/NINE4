4a7b5a042aa1b6410ffe1c6ad71b9f8fe0de2426
Jack Latimer <jacklatimer03@gmail.com>
2021-03-16 02:17:10 +0000

    done(main page): take out image border

--- components/LicenseMain.js
+++ components/LicenseMain.js
@@ -6,11 +6,19 @@ export default function Main() {
           <h2 className="mb-1 text-5xl font-bold tracking-tighter text-white lg:text-7xl md:text-6xl">
             Creative Commons Zero v1.0 Universal
           </h2>
-          <a href="https://creativecommons.org/publicdomain/zero/1.0/" className="text-white py-2">https://creativecommons.org/publicdomain/zero/1.0/</a>
+          <a
+            href="https://creativecommons.org/publicdomain/zero/1.0/"
+            className="text-white py-2"
+          >
+            https://creativecommons.org/publicdomain/zero/1.0/
+          </a>
           <p className="mx-auto text-xl font-normal leading-relaxed text-middle fs521 lg:w-2/3">
-          The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
-
-You can copy, modify, distribute and perform the work, even for commercial purposes, all without asking permission.
+            The person who associated a work with this deed has dedicated the
+            work to the public domain by waiving all of his or her rights to the
+            work worldwide under copyright law, including all related and
+            neighboring rights, to the extent allowed by law. You can copy,
+            modify, distribute and perform the work, even for commercial
+            purposes, all without asking permission.
           </p>
         </div>
       </div>
