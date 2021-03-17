2081c8673f557fcba6d177cf62cc724131c0b172
Jack Latimer <jacklatimer03@gmail.com>
2021-03-17 10:58:49 +0000

    done(favicon & SEO): fixed favicon & descriptions

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import { Helmet } from "react-helmet";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Helmet htmlAttributes={{ lang: "en" }} />
       <Component {...pageProps} />
     </ThemeProvider>
