c2b26b6572b44cfcdd8df3c680495fc436be5580
Jack Latimer <jacklatimer03@gmail.com>
2021-05-25 21:28:09 +0100

    done(images): using next/images and fixed boxes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-    env: {
-      GA_ENV: process.env.GA_ENV,
-    },
-    webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: "empty",
-        };
-      }
-  
-      return config;
-    },
-  };
+  images: {
+    domains: ["nine4.app"],
+  },
+  env: {
+    GA_ENV: process.env.GA_ENV,
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
+};
