ac382d98a525f3a5541efc2d9e8ce7040a0772ef
Jack Latimer <jacklatimer03@gmail.com>
2022-03-26 02:23:20 +0000

    done(SEO): changed title

--- pages/license.js
+++ pages/license.js
@@ -16,7 +16,7 @@ export default function license() {
           ></meta>
         </Head>
         <NextSeo
-          title="nine4 - Free templates built with Next.js and styled with Tailwind CSS"
+          title="nine4.app - Free templates built with Next.js and styled with Tailwind CSS"
           description="nine4 is a free to use template website with free templates for professional and personal use."
         />
         <Header />
