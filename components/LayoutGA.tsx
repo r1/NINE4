f2aacf5b6e27b102d63863fd2e7aecfe23e042e7
Jack Latimer <jacklatimer03@gmail.com>
2022-03-26 01:59:15 +0000

    done(ts): extend global interface fix

--- components/LayoutGA.tsx
+++ components/LayoutGA.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { initGA, logPageView } from "../utils/analytics";
 
+declare global {
+  interface Window {
+    GA_INITIALIZED:any;
+  }
+}
+
 export default class Layout extends React.Component {
+  
   componentDidMount() {
     if (!window.GA_INITIALIZED) {
       initGA();
